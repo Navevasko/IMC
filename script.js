8e6ea5a474ea81966b565fb4cebff46f348a880a
GUILHERME NAVEVAIKO MOREIRA <21193389@CFP127ALUNO.LOCAL>
2021-08-17 16:37:01 -0300

    Correção

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ function imc () {
     const resultado = document.getElementById("resultado")
     
     if (txtNome !== '' && txtAltura !== '' && txtPeso !== '') {
-        const valorIMC = (txtPeso / (txtAltura * txtAltura)).toFixed(1) // Execução do cálculo usando .toFixed() para controlar as casas deciamis.
+        const valorIMC = (txtPeso / (txtAltura * txtAltura)).toFixed(1)
         
         let classificacao = '' 
         
-        if (valorIMC < 18.5) {
+        if (valorIMC < 18.5 ) {
             classificacao = 'abaixo do peso'
-        } else if (valorIMC < 24.9) {
+        } else if (valorIMC < 24.9 && valorIMC >= 18.5) {
             classificacao = 'com peso ideal. Parabéns!!!'
-        } else if (valorIMC < 29.9) {
+        } else if (valorIMC < 29.9 && valorIMC >= 25.0) {
             classificacao = 'acima do peso'
-        } else if (valorIMC < 34.9) {
+        } else if (valorIMC < 34.9 && valorIMC >= 30.0) {
             classificacao = 'com obesidade grau I'
         } else if (valorIMC < 40) {
             classificacao = 'com obesidade grau II'
-        } else {
-            classificacao = 'com obesidade grau III. Cuidado!!!'
+        } else if(valorIMC > 40) {
+            classificacao = 'com obesidade grau III.'
         }
         
         resultado.textContent = `${txtNome} seu IMC é ${valorIMC} você está ${classificacao}`  
